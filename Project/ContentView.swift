7d9e403dbfc51b4d7cc847634a37d5cec6561b28
Ruth-Ann Armstrong <ruth-ann.armstrong@outlook.com>
2020-04-12 10:40:23 -0700

    new lines added

--- Project/ContentView.swift
+++ Project/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello!")
+  
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
+    
+    //check2
 }
