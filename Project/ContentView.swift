76b3227c304f7edf14c62859cb603284ee0b3b0f
Ruth-Ann Armstrong <ruth-ann.armstrong@outlook.com>
2020-04-12 10:12:59 -0700

    Initial Commit

---
+++ Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by Ruth-Ann Armstrong on 4/12/20.
+//  Copyright © 2020 Ruth-Ann Armstrong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
